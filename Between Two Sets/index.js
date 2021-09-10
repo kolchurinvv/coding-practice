cedc6351ac061a770bcac1ff6697771e40e48ec1
Vladimir Kolchurin <kolchurin@gmail.com>
2021-09-10 13:12:13 +0500

    looked up solution for Between Two Sets

--- Between Two Sets/index.js
+++ Between Two Sets/index.js
@@ -13,28 +13,45 @@ let input = readFileSync("input.txt", "utf8", (err, data) => {
 
 let [[n, m], a, b] = input;
 
+// This is my solution; the problem is the update of the length of an array using splice()
+// when it comes to the last element in the spliced array
+
+// function getTotalX(a, b) {
+//   const boundaries = [parseInt(a.slice(-1)), parseInt(b.slice(0, 1))];
+//   let possibleVals = [];
+//   for (let i = boundaries[0]; i <= boundaries[1]; i++) {
+//     possibleVals.push(i);
+//   }
+//   for (const valA of a) {
+//     for (const x of possibleVals) {
+//       console.log(x, valA);
+//       if (x % valA) {
+//         possibleVals.splice(possibleVals.indexOf(x), 1);
+//       }
+//     }
+//   }
+// for (const valB of b) {
+//   for (const x of possibleVals) {
+//     if (valB % x) {
+//       possibleVals.splice(possibleVals.indexOf(x), 1);
+//     }
+//   }
+// }
+//   return possibleVals;
+// }
+
 function getTotalX(a, b) {
-  const boundaries = [parseInt(a.slice(-1)), parseInt(b.slice(0, 1))];
-  let possibleVals = [];
-  for (let i = boundaries[0]; i <= boundaries[1]; i++) {
-    possibleVals.push(i);
-  }
-  for (const valA of a) {
-    for (const x of possibleVals) {
-      if (x % valA) {
-        possibleVals.splice(possibleVals.indexOf(x), 1);
-      }
-    }
-  }
-  for (const valB of b) {
-    for (const x of possibleVals) {
-      if (valB % x) {
-        possibleVals.splice(possibleVals.indexOf(x), 1);
+  let validCount = 0;
+
+  for (let x = 1; x <= 100; x++) {
+    if (a.every((int) => x % int == 0)) {
+      if (b.every((int) => int % x == 0)) {
+        validCount++;
       }
     }
   }
 
-  return { possibleVals };
+  return validCount;
 }
 console.time("getTotalX");
 console.log(getTotalX(a, b));
